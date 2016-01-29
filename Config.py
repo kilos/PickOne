ec2939158eb2cbcd5407e602246305e7ff2843a4
DAVID SHAO <szshao@MacBook-Pro-3.local>
2016-01-28 22:20:21 -0600

    new file:   Config.py 	modified:   PickOne.py 	renamed:    tickers.csv -> Tickers.csv

---
+++ Config.py
@@ -0,0 +1,3 @@
+TickerFilePath = "./Tickers.csv"
+TimeBegin = "2016-01-01"
+TimeEnd = "2016-01-28"
--- PickOne.py
+++ PickOne.py
@@ -2,11 +2,22 @@
 import numpy
 import scipy
 import datetime
-import 
+import Config
 
 def main():
-  tickers = readTickerFile()
+  tickers = readTickerFile(Config.TickerFilePath)
   for t in tickers:
-    ts = readData(t)
+    ts = getStockPrice(t, Config.TimeBegin, Config.TimeEnd)
     res = analyzer(ts)
   return 0
+
+def readTickerFile(filepath):
+  lst = []
+  with open(filepath, 'r') as f:
+    for line in f:
+      line = line.rstrip()
+      if line[0] == '#':
+        continue
+      lst.append(line)
+
+  return lst
